7aa8ed2e1300a90875d45e0539921858331804b8
Sairaj Patil <sairajpatil035@gmail.com>
2026-08-27 22:43:31 +0530

    Hero section

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -3,7 +3,50 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+       <div className='container mt-5'>
+        <div className='row'>
+
+       <div className='col-6 p-5'>
+        <img src='media/images/largestBroker.svg'></img>
+       </div>
+       <div className='col-6 p-5 mt-5'>
+        <h1>Largest stock broker in India</h1>
+        <p className='mb-5' >2+ million zerodha clients to over 15% of all retail order volumes in India daily by trading and investing in:
+        </p>
+         <div className='row'>
+           <div className='col-6'>
+             <ul>
+          <li>
+            <p>Future and Options</p>
+          </li>
+          <li>
+            <p>Commodity derivatives</p>
+          </li>
+          <li>
+            <p>Currency Derevatives</p>
+          </li>
+        </ul>
+           </div>
+
+           <div className='col-6'>
+             <ul>
+          <li>
+            <p>Stock and Options</p>
+          </li>
+          <li>
+            <p>Direct mutal funds</p>
+          </li>
+          <li>
+            <p>Bonds and Govt. Securities</p>
+          </li>
+        </ul>
+           </div>
+         </div>
+         <img src='media/images/pressLogos.png' style={{width:'95%'}} ></img>
+       </div>
+
+        </div>
+       </div>
      );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>
-            Hero <i class="fa fa-user-circle-o" aria-hidden="true"></i>
-        </h1>
+       <div className='container p-5 mb-5' >
+        <div className='row text-center'> 
+            <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' ></img>
+            <h1 className='mt-5'>
+               Invest in everything
+            </h1>
+            <p>
+               Online platform to  invest in stocks, derivatives, mutual funds, and more
+            </p>
+            <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:'20%', margin:'0 auto'}}>Sign Up Now</button>
+            </div> 
+
+       </div>
      );
 }
 
