815009f8df8bf08e9b9ea836f4a79422a3b0c901
Sairaj Patil <sairajpatil035@gmail.com>
2026-09-02 09:16:29 +0530

    Learning Advanced  Dropdowns

--- frontend/src/landing_page/Support/CreateTicket.js
+++ frontend/src/landing_page/Support/CreateTicket.js
@@ -2,43 +2,236 @@ import React from 'react';
 
 function CraeteTicket() {
     return ( 
-      <div className='container'>
+      <div className='container p-5 border-top'>
 
-       
+      
         
 
 
 
             <div className='row'>
             <div className='col'>
-             
-             {/* Card 1 - Open by default */}
-            <div className="border rounded-1 mb-3 bg-light overflow-hidden" style={{ borderColor: '#eee' }}>
-                <div 
-                    className="d-flex justify-content-between align-items-center p-3" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#box1"
-                    style={{ cursor: 'pointer' }}
+
+             {/*1  Understanding this card */}
+             <div className='border rounded-1 mb-3 bg-light overflow-hidden' style={{borderColor:'#eee'}}>
+               <div 
+               className='d-flex justify-content-between 
+               align-items-center p-3'
+               data-bs-toggle="collapse"
+               data-bs-target="#box7"
+               style={{cursor:'pointer'}}
                 >
-                    <div className="d-flex align-items-center ms-2">
-                        <i className="fa fa-plus-circle text-primary me-3 fs-5"></i>
-                        <span className="fw-medium text-dark fs-6">Account Opening</span>
-                    </div>
-                    <i className="fa fa-chevron-down text-muted fs-6 me-2"></i>
+                <div className='d-flex align-items-center ms-2'>
+                   <i className='fa fa-plus-circle text-primary me-3 fs-5'></i>
+                   <span className='fw-medium text-dark fs-6'>Account Opening</span>
                 </div>
+                <i className='fa fa-chevron-down text-muted fs-6 me-2' ></i>
+               </div>
+              <div className='collapse show bg-white border-top p-4' id='box7'>
+                <ul className='mb-0 ps-3' style={{color:'#387ed1'}}>
 
-                <div className="collapse show bg-white border-top p-4" id="box1">
-                    <ul className="mb-0 ps-3" style={{ color: '#387ed1' }}>
-                        <li className="mb-2"><a href="#" className="text-decoration-none" style={{ color: '#387ed1' }}>Resident individual</a></li>
-                        <li className="mb-2"><a href="#" className="text-decoration-none" style={{ color: '#387ed1' }}>Minor</a></li>
-                        <li className="mb-2"><a href="#" className="text-decoration-none" style={{ color: '#387ed1' }}>Non Resident Indian (NRI)</a></li>
-                        <li className="mb-2"><a href="#" className="text-decoration-none" style={{ color: '#387ed1' }}>Company, Partnership, HUF and LLP</a></li>
-                        <li className="mb-2"><a href="#" className="text-decoration-none" style={{ color: '#387ed1' }}>Glossary</a></li>
-                    </ul>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Resident individual</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Minor </a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Non Resident Indian (NRI)</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Company, Partnership, HUF and LLP</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                     Glossary</a></li>
+                    
+                </ul>
+              </div>
+
+
+
+             </div>
+
+
+
+
+
+
+
+
+
+
+ {/* 2 Understanding this card */}
+             <div className='border rounded-1 mb-3 bg-light overflow-hidden' style={{borderColor:'#eee'}}>
+               <div 
+               className='d-flex justify-content-between 
+               align-items-center p-3'
+               data-bs-toggle="collapse"
+               data-bs-target="#box2"
+               style={{cursor:'pointer'}}
+                >
+                <div className='d-flex align-items-center ms-2'>
+                   <i className='fa fa-plus-circle text-primary me-3 fs-5'></i>
+                   <span className='fw-medium text-dark fs-6'>Your Zerodha Account</span>
                 </div>
-            </div>
+                <i className='fa fa-chevron-down text-muted fs-6 me-2' ></i>
+               </div>
+              <div className='collapse show bg-white border-top p-4' id='box2'>
+                <ul className='mb-0 ps-3' style={{color:'#387ed1'}}>
+
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Your Profile</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Account modification</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Client Master Report (CMR) and Depository Participant (DP)</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Nomination</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                     Transfer and conversion of securities</a></li>
+                    
+                </ul>
+              </div>
 
 
+
+             </div>
+              {/* Understanding this card */}
+             <div className='border rounded-1 mb-3 bg-light overflow-hidden' style={{borderColor:'#eee'}}>
+               <div 
+               className='d-flex justify-content-between 
+               align-items-center p-3'
+               data-bs-toggle="collapse"
+               data-bs-target="#box3"
+               style={{cursor:'pointer'}}
+                >
+                <div className='d-flex align-items-center ms-2'>
+                   <i className='fa fa-plus-circle text-primary me-3 fs-5'></i>
+                   <span className='fw-medium text-dark fs-6'>Kite </span>
+                </div>
+                <i className='fa fa-chevron-down text-muted fs-6 me-2' ></i>
+               </div>
+              <div className='collapse show bg-white border-top p-4' id='box3'>
+                <ul className='mb-0 ps-3' style={{color:'#387ed1'}}>
+
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>IPO</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Trading FAQs</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Margin Trading Facility (MTF) and Margins
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Charts and orders
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                    Alerts and Nudges
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                   General
+</a></li>
+
+                    
+                </ul>
+              </div>
+
+
+
+             </div>
+              {/* Understanding this card */}
+             <div className='border rounded-1 mb-3 bg-light overflow-hidden' style={{borderColor:'#eee'}}>
+               <div 
+               className='d-flex justify-content-between 
+               align-items-center p-3'
+               data-bs-toggle="collapse"
+               data-bs-target="#box4"
+               style={{cursor:'pointer'}}
+                >
+                <div className='d-flex align-items-center ms-2'>
+                   <i className='fa fa-plus-circle text-primary me-3 fs-5'></i>
+                   <span className='fw-medium text-dark fs-6'>Funds</span>
+                </div>
+                <i className='fa fa-chevron-down text-muted fs-6 me-2' ></i>
+               </div>
+              <div className='collapse show bg-white border-top p-4' id='box4'>
+                <ul className='mb-0 ps-3' style={{color:'#387ed1'}}>
+
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Add money
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}> Withdraw money
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Add bank accounts
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>eMandates</a></li>
+                    
+                    
+                </ul>
+              </div>
+
+
+
+             </div>
+              {/* Understanding this card */}
+             <div className='border rounded-1 mb-3 bg-light overflow-hidden' style={{borderColor:'#eee'}}>
+               <div 
+               className='d-flex justify-content-between 
+               align-items-center p-3'
+               data-bs-toggle="collapse"
+               data-bs-target="#box5"
+               style={{cursor:'pointer'}}
+                >
+                <div className='d-flex align-items-center ms-2'>
+                   <i className='fa fa-plus-circle text-primary me-3 fs-5'></i>
+                   <span className='fw-medium text-dark fs-6'>Console</span>
+                </div>
+                <i className='fa fa-chevron-down text-muted fs-6 me-2' ></i>
+               </div>
+              <div className='collapse show bg-white border-top p-4' id='box5'>
+                <ul className='mb-0 ps-3' style={{color:'#387ed1'}}>
+
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Portfolio</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Corporate actions
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Funds statement
+
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Reports</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                     Profile</a></li>
+
+                     <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                     Segments</a></li>
+                    
+                
+                </ul>
+              </div>
+
+
+
+             </div>
+              {/* Understanding this card */}
+             <div className='border rounded-1 mb-3 bg-light overflow-hidden' style={{borderColor:'#eee'}}>
+               <div 
+               className='d-flex justify-content-between 
+               align-items-center p-3'
+               data-bs-toggle="collapse"
+               data-bs-target="#box6"
+               style={{cursor:'pointer'}}
+                >
+                <div className='d-flex align-items-center ms-2'>
+                   <i className='fa fa-plus-circle text-primary me-3 fs-5'></i>
+                   <span className='fw-medium text-dark fs-6'>Coin</span>
+                </div>
+                <i className='fa fa-chevron-down text-muted fs-6 me-2' ></i>
+               </div>
+              <div className='collapse show bg-white border-top p-4' id='box6'>
+                <ul className='mb-0 ps-3' style={{color:'#387ed1'}}>
+
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Mutual funds
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>National Pension Scheme (NPS)
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Fixed Deposit (FD)
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>Features on Coin
+</a></li>
+                    <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+                     Payments and Orders
+</a></li>            
+                     <li className='mb-3'><a href='#' className='text-decoration-none' style={{color:'387ed1'}}>
+General
+</a></li>
+                    
+                </ul>
+              </div>
+
+
+
+             </div>
         </div> 
 
         
