884df9eba7706f0325d42bed640054472612a4b3
Sairaj Patil <sairajpatil035@gmail.com>
2026-09-11 12:40:40 +0530

    backend

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,7 @@
+const { Schema } = require("mongoose");
+
+const { PositionsSchema } = require('../schemas/PositionsSchema');
+
+const PositionsModel = new model('Position', PositionsSchema );
+
+module.exports = {PositionsModel};
