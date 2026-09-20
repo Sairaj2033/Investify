f4918339f3e32b3479a86220869c934540a82a6e
Sairaj Patil <sairajpatil035@gmail.com>
2026-09-20 18:35:21 +0530

    Giving a final touch 1

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -12,9 +12,9 @@ import Bids from './Bids';
 
 function Dashboard() {
   return ( 
-    <div className='dashboard-container' style={{ display: 'flex', width: '100%',border:'1px solid black' }}>
+    <div className='dashboard-container' style={{ display: 'flex', width: '100%' }}>
       {/* LEFT PANEL */}
-      <div style={{ width: '29.2%', borderRight: '1px solid #eeeaea', border:'1px solid black',paddingLeft:'2rem'}}>
+      <div style={{ width: '29.2%', borderRight: '1px solid #eeeaea',paddingLeft:'2rem'}}>
         <Watchlist />
       </div> 
       
